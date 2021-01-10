af8f25a843ea091bd30852abfe10e81d1987aee6
seiya endo <seiya.endo0830@gmail.com>
2021-01-10 13:32:50 +0900

    add: Express routeing

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,22 @@
-// import express from 'express'
-const express = require('express')
+import express from 'express'
+// const express = require('express')
 
 const app = express()
+
+// middleware
+app.use((req, res, next) => {
+  console.log('hello')
+  next()
+})
+
+// route
+app.get('/', (req, res, next) => {
+  res.send('<h1>Hello</h1>')
+})
+
+// error handling
+// app.use((err, req, res, next) => {
+//   next()
+// })
+
 app.listen(3000)
